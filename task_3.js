8a7530b1c28dcd1c1cf4244738036cd6f136859f
Katsiaryna Rubanik <gourdmand@gmail.com>
2023-03-06 17:01:05 +0100

    Task 3 updated

--- task_3.js
+++ task_3.js
@@ -1,20 +1,19 @@
 // Составить алгоритм: на входе есть числовой массив,
 // необходимо вывести элементы массива кратные 3
 
-function numericFilter (arrayBefore) {
-    return arrayBefore.filter(item => (item % 3 === 0))
-}
-
 const prompt = require('prompt-sync')();
-let numericArray = prompt('Enter the array items separated by a space: ');
-numericArray = numericArray.trim().split(` `);
 
-const numericArrayAfterFilter = numericFilter(numericArray);
+const promptData = prompt('Enter the array items separated by a space: ');
+const arrayFromPrompt = promptData
+    .trim()
+    .split(` `);
+const arrayFromPromptFiltered = arrayFromPrompt
+    .filter(item => (item % 3 === 0));
 
-console.log(`Your array: [${numericArray}]`);
+console.log(`Your array: [${arrayFromPrompt}]`);
 
-if (numericArrayAfterFilter.length === 0)   {
+if (arrayFromPromptFiltered.length === 0)   {
     console.log(`There are no elements multiples 3 in your array`)
 }   else {
-    console.log(`Array after being filtered is: [${numericArrayAfterFilter}]`)
+    console.log(`Array after being filtered is: [${arrayFromPromptFiltered}]`)
 }
