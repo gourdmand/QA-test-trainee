3bcfca0edc4d7820fdecd7aad4697ca2a7a74fe1
Katsiaryna Rubanik <gourdmand@gmail.com>
2023-03-06 14:37:43 +0100

    Task 2 completed

---
+++ task_2.js
@@ -0,0 +1,12 @@
+// Составить алгоритм: если введенное имя совпадает с Вячеслав,
+// то вывести “Привет, Вячеслав”, если нет, то вывести "Нет такого имени"
+
+const prompt = require('prompt-sync')();
+const enteredName = prompt('Enter the name: ');
+console.log(`You entered '${enteredName}'`);
+
+if (enteredName === `Вячеслав`) {
+    console.log(`Привет, ${enteredName}`);
+}   else {
+    console.log(`Нет такого имени`);
+}
